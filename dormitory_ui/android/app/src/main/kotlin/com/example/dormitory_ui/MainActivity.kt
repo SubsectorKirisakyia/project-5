d238d15d797ec0fef8c769dba247c34b3fd0f673
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2025-07-02 09:54:26 +0700

    Pushing my own prototype for frontend to a new branch.

---
+++ dormitory_ui/android/app/src/main/kotlin/com/example/dormitory_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dormitory_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ dormitory_ui/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'register_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spring Boot Auth',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+      },
+    );
+  }
+}
